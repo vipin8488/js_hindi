26c6be9be65ebd02af6273720a7c818632c4d316
vipin8488 <vipinjangra8488@gmail.com>
2025-02-07 05:11:00 +0000

    container congi done

--- 01/first.js
+++ 01/first.js
@@ -1,1 +1,16 @@
 console.log ("hellleoo")
+
+const accountId = 1222   // const use to lock the value
+let accountEmail = "hitesh@google.com"  //it can be changed
+var accountPassword = "5555"  //we will not use it mostly we will use instesd let
+
+
+accountcity = "jaipur" //declaration varaile like this can done but this is not good practice to do
+
+accountEmail="jfkjjf   "
+accountPassword="12456"
+let account;
+
+accountcity = "gurgaon"
+console.log( [accountId, accountEmail, accountPassword ] );   //when you want to print in the list formate then just use [] and inbetween square brackets write the variable, then the solution will print in list formate
+console.table( [ accountcity,accountEmail, accountPassword,account])   // by using .table you will able to print in a tabular form
---
+++ 01/second.js
@@ -0,0 +1,21 @@
+/*
+their re many primitive datatypes :
+number 
+bigint
+string
+boolean
+null=>it is standalone value
+undefined => it shows that vriable ki value is not defined
+symbol => unique
+
+to find datatype of any variable we use kryword=> typeof 
+*datatype of null is object given by js this can says system issue or langyage problem because null is not object
+*datatype of undefined is undefind, this is also weird 
+*/
+
+let car="supra"
+let ball = null
+let bike;
+console.log(typeof car)  //check output of all this datatype
+console.log(typeof ball)
+console.log(typeof bike)
